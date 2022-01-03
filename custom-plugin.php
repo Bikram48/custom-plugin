c6635505055e3f062800ed7372071a6eff282b8a
Bikram48 <chandbikram001@gmail.com>
2022-01-03 14:39:41 +0545

    plugin files

---
+++ custom-plugin.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Plugin Name: Custom Plugin
+ * Description: This plugin designed for user authentication and authroization.
+ * Version: 1.0.0
+ * Requires at least: 5.1.0
+ * Requires PHP: 7.0.0
+ * Author: Bikram
+ * Author URI: https://bikram.com
+ * License: GNU GPL 
+ * License URI: https://license.com
+ * Text Domain: CustomTxt
+ * Domain Path: /languages
+ */
