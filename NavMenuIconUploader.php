b06db8779e51c0e30a9ac08f7ada50ca0a798540
Bikram48 <bikram.chand@themegrill.com>
2022-01-05 17:34:23 +0545

    add - action hook to update menu item data

--- NavMenuIconUploader.php
+++ NavMenuIconUploader.php
@@ -9,19 +9,29 @@
             }
 
             public static function init_action() {
-                add_action( 'wp_nav_menu_item_custom_fields', array( __CLASS__, 'add_upload_option' ) );
+                add_action( 'wp_nav_menu_item_custom_fields', array( __CLASS__, 'add_upload_option' ), 10, 2 );
                 add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_script' ) );
+                add_action( 'wp_update_nav_menu_item', array( __CLASS__, 'save_menu_item' ) );
             }
 
-            public static function add_upload_option( $item_id ) { ?>
+            public static function add_upload_option( $item_id, $item ) { 
+                error_log( print_r( $item, true ) );
+                ?>
                 <p class="menu-icon=">
 					<label for="menu-icon-upload-<?php echo $item_id; ?>">
-						<?php _e( 'Description' ); ?><br />
-						<textarea id="menu-icon-upload-<?php echo $item_id; ?>" class="widefat edit-menu-item-description" rows="3" cols="20" name="menu-icon[<?php echo $item_id; ?>]"><?php echo esc_html( $item->description ); // textarea_escaped ?></textarea>
-						<span class="description"><?php _e( 'The description will be displayed in the menu if the current theme supports it.' ); ?></span>
+						<?php _e( 'Upload Icon', 'custom-plugin' ); ?><br />
+						<textarea id="menu-icon-upload-<?php echo $item_id; ?>" 
+                        class="menu-icon-upload" rows="3" cols="20" 
+                        name="menu-icon[<?php echo $item_id; ?>]">
+                        <?php echo esc_html( $item->description ); // textarea_escaped ?></textarea>
+						
 					</label>
 				</p>
 <?php       }
+
+            public static function save_menu_item() {
+
+            }
 
             public static function enqueue_script() {
                 wp_enqueue_media();
@@ -29,5 +39,3 @@
             }
         }
     }
-      
-    
